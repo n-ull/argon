4c01418cc5eb27e066b141458c5cb3de93414fc3
Nicolas Carol <felix.greed@gmail.com>
2025-11-16 09:25:14 -0300

    feat: added fillables

--- src/Domain/ProductCatalog/Models/Product.php
+++ src/Domain/ProductCatalog/Models/Product.php
@@ -7,7 +7,23 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
-    public function product_prices(): HasMany {
+    protected $fillable = [
+        "name",
+        "description",
+        "max_per_order",
+        "min_per_order",
+        "product_type",
+        "hide_before_sale_start_date",
+        "hide_after_sale_end_date",
+        "hide_when_sold_out",
+        "show_stock",
+        "start_sale_date",
+        "end_sale_date",
+        "event_id",
+    ];
+
+    public function product_prices(): HasMany
+    {
         return $this->hasMany(ProductPrice::class)->orderBy('sort_order');
     }
 }
--- src/Domain/ProductCatalog/Models/ProductPrice.php
+++ src/Domain/ProductCatalog/Models/ProductPrice.php
@@ -7,6 +7,18 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProductPrice extends Model
 {
+    protected $fillable = [
+        "product_id",
+        "price",
+        "label",
+        "start_sale_date",
+        "end_sale_date",
+        "stock",
+        "quantity_sold",
+        "is_hidden",
+        "sort_order",
+    ];
+    
     public function product(): BelongsTo {
         return $this->belongsTo(Product::class);
     }
