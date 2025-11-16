d52f27e5509d7d612253cd8c61c98384fee4e42f
Nicolas Carol <felix.greed@gmail.com>
2025-11-16 09:45:43 -0300

    feat: added docstrings and validator service for events

--- src/Domain/EventManagment/Models/Event.php
+++ src/Domain/EventManagment/Models/Event.php
@@ -9,6 +9,26 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * Event Model
+ * 
+ * Represents an event in the event management system.
+ * 
+ * @property int $id
+ * @property string $title The title of the event
+ * @property string|null $description The description of the event
+ * @property array $location_info JSON object containing location information
+ * @property EventStatus $status The current status of the event
+ * @property \Carbon\Carbon $start_date The start date and time of the event
+ * @property \Carbon\Carbon $end_date The end date and time of the event
+ * @property int $organizer_id The ID of the organizer
+ * @property bool $is_featured Whether the event is featured
+ * @property string $slug The URL slug for the event
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * 
+ * @property-read Organizer $organizer The organizer of this event
+ */
 class Event extends Model
 {
     use HasFactory;
---
+++ src/Domain/EventManagment/Services/EventValidatorService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Domain\EventManagment\Services;
+
+use Domain\EventManagment\Models\Event;
+
+class EventValidatorService
+{
+    public function validate(Event $event): void
+    {
+        if ($event->start_date->gt($event->end_date)) {
+            throw new \DomainException("Event start must be before end");
+        }
+    }
+}
