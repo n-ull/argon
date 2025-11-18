c674ce652c16c9c751c3518d15fd297588d5dfbf
Nicolas Carol <felix.greed@gmail.com>
2025-11-18 14:59:40 -0300

    refactor: moved event created to the action

--- src/Domain/EventManagement/Actions/CreateEvent.php
+++ src/Domain/EventManagement/Actions/CreateEvent.php
@@ -3,6 +3,8 @@
 namespace Domain\EventManagement\Actions;
 
 use App\Modules\EventManagement\Requests\StoreEventRequest;
+use Domain\EventManagement\Events\EventCreated;
+use Domain\EventManagement\Models\Event;
 use Domain\EventManagement\Services\EventManagerService;
 use Lorisleiva\Actions\Concerns\AsAction;
 
@@ -12,10 +14,15 @@ class CreateEvent
 
     public function __construct(
         private EventManagerService $eventManager
-    ) {}
+    ) {
+    }
 
-    public function handle(array $data)
+    public function handle(array $validatedData): Event
     {
-        return $this->eventManager->createEvent($data);
+        $event = $this->eventManager->createEvent($validatedData);
+
+        event(new EventCreated($event));
+
+        return $event;
     }
 }
