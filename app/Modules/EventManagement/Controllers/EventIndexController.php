cee54e579c821af60b1b7111d9ca15cd34f44490
Nicolas Carol <felix.greed@gmail.com>
2025-11-20 13:11:17 -0300

    feat: added query selection to retrieve only the correct events

--- app/Modules/EventManagement/Controllers/EventIndexController.php
+++ app/Modules/EventManagement/Controllers/EventIndexController.php
@@ -12,8 +12,11 @@ class EventIndexController extends Controller
     public function index()
     {
         $events = EventModel::orderBy("start_date", "desc")
+        ->where("start_date", ">=", now())
+        ->where("status", EventStatus::PUBLISHED)
+        ->where("is_featured", true)
         ->paginate(10);
-
+ 
         return Inertia::render("events/Index", [
             "events"=> Inertia::scroll($events)
         ]);
