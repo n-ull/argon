4a4d70a9c711c4d316ee65bd1a42503440b9a996
Nicolas Carol <felix.greed@gmail.com>
2025-12-15 21:47:54 -0300

    feat: make dashboard event titles clickable and update logo text color to white.

--- resources/js/layouts/auth/AuthSimpleLayout.vue
+++ resources/js/layouts/auth/AuthSimpleLayout.vue
@@ -16,7 +16,7 @@ defineProps<{
                 <div class="flex flex-col items-center gap-4">
                     <Link :href="home()" class="flex flex-col items-center gap-2 font-medium">
                         <div class="mb-1 flex h-9 w-9 items-center justify-center rounded-md">
-                            <AppLogoIcon class="size-9 fill-current text-[var(--foreground)] dark:text-white" />
+                            <AppLogoIcon class="size-9 fill-current text-white" />
                         </div>
                         <span class="sr-only">{{ title }}</span>
                     </Link>
