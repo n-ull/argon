b2ff17320a099af6cf7a41de84f5c2c7bb5573bc
Nicolas Carol <felix.greed@gmail.com>
2026-01-06 09:18:22 -0300

    feat: more assertions in the create order test

--- tests/Unit/Domain/Ticketing/Actions/CreateOrderTicket.php
+++ tests/Unit/Domain/Ticketing/Actions/CreateOrderTicket.php
@@ -6,9 +6,18 @@ use Domain\Ticketing\Models\Ticket;
 test('domain/ticketing/actions/createorderticket', function () {
     $product = setupAvailableProduct();
 
-    $action = CreateOrderTicket::run([
-        'product_id' => $product->id,
-    ]);
+    $action = CreateOrderTicket::run($product);
 
     expect($action)->toBeInstanceOf(Ticket::class);
+    expect($action->token)->not()->toBeEmpty();
+    expect($action->event_id)->toBe($product->event_id);
+    expect($action->product_id)->toBe($product->id);
+    expect($action->order_id)->toBe($product->order_id);
+
+    $this->assertDatabaseHas('tickets', [
+        'token' => $action->token,
+        'event_id' => $product->event_id,
+        'product_id' => $product->id,
+        'order_id' => $product->order_id,
+    ]);
 });
